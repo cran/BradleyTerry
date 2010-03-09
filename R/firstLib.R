95bda5286a29d6ccd5bc4e987606a4a0d7de2b8b
David Firth <d.firth@warwick.ac.uk>
2010-03-09 00:00:00 +0000

    version 0.8-8

---
+++ R/firstLib.R
@@ -0,0 +1,7 @@
+.First.lib <- function (lib, pkg) 
+{
+    packageStartupMessage("Loading the 'BradleyTerry' package...", "\n",
+	"Note that this package is now deprecated, please consider", "\n",
+	"using the 'BradleyTerry2' package instead!", 
+        appendLF = TRUE)
+} 
